d13574c86484edc4c8118bde8c795f4f0843bbae
Martin R. Smith <martins@gmail.com>
2016-04-11 10:24:23 +0100

    Data
    Document data.  Load lazily.

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' 100 simulated data matrices
+#'
+#' A dataset 100 matrices simulated on a reference tree using an Mk 1 state model
+#' with heterogeneous rates.
+#'
+#' @format A list with 100 entries, each comprising a phyDat object of 55 characters for 22 taxa
+#'
+#' @source \url{http://datadryad.org/resource/doi:10.5061/dryad.7dq0j}
+"congreve.lamsdell.matrices"
+
+#' Tree topology for simulation
+#'
+#' The tree topology used to generate the matrices
+#'
+#' @format A single phylogenetic tree saved as an object of class phylo
+#'
+#' @source \url{http://dx.doi.org/10.1111/pala.12236}
+"reference.tree"
