ffa2684fb98ddfe407cec648085be258fd053181
Martin R. Smith <martins@gmail.com>
2017-07-26 14:37:35 +0100

    Merge out functions to TreeSearch library

--- R/tree_search.R
+++ R/tree_search.R
@@ -1,18 +1,11 @@
-Suboptimality <- function (trees, proportional = FALSE) {
-  scores <- vapply(trees, attr, double(1), 'score')
-  if (proportional) {
-    return ((scores - min(scores)) / min(scores))
-  } else {
-    return(scores - min(scores))
-  }
-}
-
 #' Profile Score
 #'
 #' @template treeParam
 #' @param data Dataset of class \code{fitchDat} 
 #' @returns Zero minus the profile score (because the optimization algorithm assumes that
 #' smaller numbers are better)
+#' @importFrom TreeSearch FitchSteps
+#' @importFrom TreeSearch TipsAreColumns
 #' @keywords internal
 #' @export
 ProfileScore <- function (tree, data) {
@@ -21,23 +14,13 @@ ProfileScore <- function (tree, data) {
     stop('Invalid data type; prepare data with PhyDat() or PrepareDataFitch().')
   }
   at <- attributes(data)
-  n.char  <- at$nr # strictly, transformation series patterns; these'll be upweighted later
+  nChar  <- at$nr # strictly, transformation series patterns; these'll be upweighted later
   weight <- at$weight
-  steps <- Fitch(tree, data, at)
+  steps <- TreeSearch::FitchSteps(tree, data, TreeSearch::TipsAreColumns, at)
   info <- at$info.amounts
-  return(-sum(info[max(0, (steps - 1)) * n.char + seq_len(n.char)] * weight))
-}
-
-SuccessiveWeights <- function(tree, data) {
-  # Data
-  if (class(data) == 'phyDat') data <- PrepareDataFitch(data)
-  if (class(data) != 'fitchDat') {
-    stop('Invalid data type; prepare data with PhyDat() or PrepareDataFitch().')
-  }
-  at <- attributes(data)
-  weight <- at$weight
-  sa.weights <- at$sa.weights
-  if (is.null(sa.weights)) sa.weights <- rep(1, length(weight))
-  steps <- Fitch(tree, data, at)
-  return(sum(steps * sa.weights * weight))
+  nColInfo <- ncol(info)
+  return (-sum(vapply(seq_len(nChar), function (i) {
+    stepCol <- max(0L, steps[i] - 1L) + 1L
+    return(if (stepCol > nColInfo) 0 else info[i, stepCol])
+  }, double(1)) * weight))
 }
