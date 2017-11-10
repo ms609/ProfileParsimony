308346925d972859acb2346fa12899e388beb399
Martin R. Smith <martins@gmail.com>
2017-11-10 16:32:34 +0000

    Fix test and propose another - though that one is somewhat trickier to calculate correct answer for!

--- tests/testthat/test-ICS.R
+++ tests/testthat/test-ICS.R
@@ -2,6 +2,7 @@ context("Information content of steps")
 
 test_that("Information content of steps calculated correctly", {
   expect_equal(as.double(ICS(2, 2, 10000) * NUnrooted(4)), c(1, 2))
-  expect_equal(signif(as.double(ICS(2, 3, 10000) * NUnrooted(5), 5)), c(2, 13))
-
+  expect_equal(signif(as.double(ICS(2, 3, 10000) * NUnrooted(5), 5)), c(3, 12))
+##  expect_equal(signif(as.double(cumsum(ICS(4, 8, 10000)), 5)), c(3, 12))
+  )
 })
